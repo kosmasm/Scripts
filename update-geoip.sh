a4536cebfc7bb4c19c1d02d2e3d1193acb894fd1
Kosmas Mackrogamvrakis <kosmas@kosmas.gr>
2025-01-23 11:40:00 +0200

    GEOIP password removal

--- update-geoip.sh
+++ update-geoip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # geoipupdate -v
-wget -q --user "512891" --password "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" "https://download.maxmind.com/geoip/databases/GeoLite2-Country-CSV/download?suffix=zip" -O /tmp/GeoIP.zip
+wget -q --user "512891" --password "xxxxxxxxxxxxxxxxx" "https://download.maxmind.com/geoip/databases/GeoLite2-Country-CSV/download?suffix=zip" -O /tmp/GeoIP.zip
 /usr/local/geolite2legacy/geolite2legacy.py -i /tmp/GeoIP.zip -o /usr/share/GeoIP/GeoIP.dat
 rm -f /tmp/GeoIP.zip
